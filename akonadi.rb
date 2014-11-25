e907440717636fe4ebc0e0f8a07496f27462c5aa
John Regan <john@jrjrtech.com>
2014-11-25 16:23:29 -0600

    akonadi: fixing compile on Mac OS X 10.8

--- akonadi.rb
+++ akonadi.rb
@@ -1,7 +1,6 @@
 require File.join(File.dirname(__FILE__), 'base_kde_formula')
 
 class Akonadi < BaseKdeFormula
-  homepage 'http://pim.kde.org/akonadi/'
   url 'http://download.kde.org/stable/akonadi/src/akonadi-1.13.0.tar.bz2'
   sha1 '9d594b5840e2e5d90057a7e5d8545004a3476bc0'
 
@@ -13,10 +12,15 @@ class Akonadi < BaseKdeFormula
 
   kde_build_deps
 
-  #def patches
-  #  {:p0 => [
-  #    "http://bugsfiles.kde.org/attachment.cgi?id=69519"
-  #  ]}
-  #end
-  
+  def patches
+    {:p0 => [
+      "https://svn.macports.org/repository/macports/trunk/dports/devel/akonadi/files/patch-tests.diff"
+    ]}
+  end
+  if MacOS.version < :mavericks
+      def extra_cmake_args
+      "-DAKONADI_BUILD_TESTS:BOOL=OFF -DBUILD_TESTING:BOOL=OFF"
+      end
+  end
+
 end
